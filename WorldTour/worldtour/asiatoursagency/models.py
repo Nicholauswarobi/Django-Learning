2480433cf1af88c1045399adc8ba2d584902bd73
Nicholauswarobi <warobinicholaus@gmail.com>
2025-06-21 02:18:51 +0300

    #

--- WorldTour/worldtour/asiatoursagency/models.py
+++ WorldTour/worldtour/asiatoursagency/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    # Define the fields for the Tour model
+    original_country = models.CharField(max_length=100)
+    destination_country = models.CharField(max_length=100)
+    number_of_nights = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # This is string represantation of tour
+    def __str__(self):
+        return (f"ID: {self.id}: from {self.original_country} To {self.destination_country}, Number of Days {self.number_of_nights} price cost {self.price} KSH")
+
--- WorldTour/worldtour/asiatoursagency/views.py
+++ WorldTour/worldtour/asiatoursagency/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Tour
+
 
 # Create your views here.
+def home(request):
+    tours_variable = Tour.objects.all()
+    context = {'tours': tours_variable}
+    return render(request, 'tours/home.html', context)
