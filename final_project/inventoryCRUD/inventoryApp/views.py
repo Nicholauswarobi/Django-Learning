15bd619e3e8ffc1242401cefd56c812bf76e9db6
Nicholauswarobi <warobinicholaus@gmail.com>
2025-06-30 00:02:07 +0300

    final project completed

--- final_project/inventoryCRUD/inventoryApp/views.py
+++ final_project/inventoryCRUD/inventoryApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ProductForm
 from .models import Product
 
@@ -16,8 +17,9 @@ def product_create_view(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Product created successfully!')
             return redirect('product_list')
-    return redirect(request, 'invApp/product_form.html', {'form':form})
+    return render(request, 'invApp/product_form.html', {'form':form})
 
 
 # Read View
@@ -28,19 +30,23 @@ def product_list_view(request):
 # Update View
 def product_update_view(request, product_id):
     product = Product.objects.get(product_id=product_id)
-    form = ProductForm()
     if request.method == "POST":
-        form = ProductForm(request.POST, instance = product)
+        form = ProductForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
+            messages.success(request, f'Product "{product.name}" updated successfully!')
             return redirect('product_list')
-    return redirect(request, 'invApp/product_form.html', {'form':form})
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'invApp/product_form.html', {'form':form})
 
 # Delete View
 def product_delete_view(request, product_id):
     product = Product.objects.get(product_id=product_id)
     if request.method == "POST":
+        product_name = product.name
         product.delete()
+        messages.success(request, f'Product "{product_name}" deleted successfully!')
         return redirect('product_list')
     
     return render(request, 'invApp/product_confirm_delete.html', {'product':product})
